b6b439a879f6f6579f06b11bb725ef417ef5b2df
xiaopch <xiaopch@163.com>
2025-02-17 09:58:27 +0800

    我立愿深入正念的生活

---
+++ 000-mkdocs_gh_deploy.bat
@@ -0,0 +1,8 @@
+::cmd  /k  E:\virtualenv\cuda\Scripts\activate
+::pip install -r requirements.txt
+::pip install pysoundfile
+::cmd  /k  E:\virtualenv\cpu\Scripts\pyinstaller -F resize-image.py  --noconsole
+cmd  /c  mkdocs gh-deploy
+cmd  /c start https://xiaopch.github.io/
+cmd /k cd .
+::sleep 80
---
+++ git_push.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git status
+git add -A
+git status
+git commit -m "我立愿深入正念的生活"
+git push
+sleep 3
